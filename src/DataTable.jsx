530b04ad6cb3f8ce8624e4a8290a6fc03a18b43f
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-09 12:49:41 +0600

    "Updated favicon, added devnet script, removed vite.svg, and modified DataTable.jsx with new input fields and table structure"

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -6,7 +6,73 @@ const DataTable = () => {
     <div>
       <div className="container">
         <div className="add-container">
-          
+          <div className="info-container">
+
+            {/* Name */}
+            <input
+              type="text"
+              placeholder='Name'
+              name='name'
+              value={""}
+              onChange={() => { }}
+            />
+
+
+            {/* Gender */}
+            <input
+              type="text"
+              placeholder='Gender'
+              name='gender'
+              value={""}
+              onChange={() => { }}
+            />
+
+            {/* Age */}
+            <input
+              type="text"
+              placeholder='Age'
+              name='age'
+              value={""}
+              onChange={() => { }}
+            />
+
+          </div>
+
+          {/* add button for adding data */}
+          <button className='add'>Add</button>
+        </div>
+
+        <div className="search-table-container">
+
+          {/* search input */}
+          <input
+            type="text"
+            placeholder='Search by name'
+            value={""}
+            onChange={() => { }}
+            className='search-input'
+          />
+
+          {/* data table showing here */}
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Action</th>
+              </tr>
+
+            </thead>
+
+            <tbody>
+
+            </tbody>
+
+            
+          </table>
+
+          {/* pagination here */}
         </div>
       </div>
 
